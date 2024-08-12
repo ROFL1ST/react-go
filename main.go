0c86217e55e9ff3af3330a07f36d85e498ad4b27
ROFL1ST <danendrapr55@gmail.com>
2024-08-12 16:32:28 +0700

    Fnished

--- main.go
+++ main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
-	"os"
-
 	"github.com/ROFL1ST/react-go/model"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
 )
 
 var collection *mongo.Collection
@@ -47,7 +47,10 @@ func main() {
 	collection = client.Database("golang_db").Collection("todos")
 
 	app := fiber.New()
-
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173", // Frontend origin
+		AllowMethods: "GET,POST,PATCH,DELETE",
+	}))
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
 	app.Patch("/api/todos/:id", updateTodos)
